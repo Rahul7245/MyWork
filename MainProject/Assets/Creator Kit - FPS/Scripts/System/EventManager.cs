4763b43e5b8c6f3ffee323f3246dde623b102e38
Rahul Maheshwari <maheshwarirohit14@gmail.com>
2020-07-13 22:41:12 +0530

    Shooting and Bird View intgrated
    1 bug : If there is ssecond animation popup comes before that animation.

---
+++ MainProject/Assets/Creator Kit - FPS/Scripts/System/EventManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.Events;
+
+public static class EventManager
+{
+    static ImpactManager shotInvoker;
+    static UnityAction<int> shotListener;
+    static ImpactManager cameraSwitchInvoker;
+    static UnityAction<bool> cameraSwitchListener;
+
+    static TrackSpawner reloadWeaponInvoker;
+    static UnityAction reloadWeaponListener;
+
+    public static void  AddShotInvoker(ImpactManager script) 
+    {
+        shotInvoker = script;
+        if (shotListener != null) {
+            shotInvoker.AddShotEventListener(shotListener);
+        }
+    }
+
+    public static void AddShootListener(UnityAction<int> listener_to_register)
+    {
+        shotListener = listener_to_register;
+        if (shotInvoker != null)
+        {
+            shotInvoker.AddShotEventListener(shotListener);
+        }
+    }
+    public static void AddCameraSwitchInvoker(ImpactManager script)
+    {
+        cameraSwitchInvoker = script;
+        if (cameraSwitchListener != null)
+        {
+            cameraSwitchInvoker.AddSwitchListener(cameraSwitchListener);
+        }
+    }
+
+    public static void AddCameraSwitchtListener(UnityAction<bool> listener_to_register)
+    {
+        cameraSwitchListener = listener_to_register;
+        if (cameraSwitchInvoker != null)
+        {
+            cameraSwitchInvoker.AddSwitchListener(cameraSwitchListener);
+        }
+    }
+
+
+
+    public static void AddReloadWeaponInvoker(TrackSpawner script)
+    {
+        reloadWeaponInvoker = script;
+        if (reloadWeaponListener != null)
+        {
+            reloadWeaponInvoker.AddResetWeaponListener(reloadWeaponListener);
+        }
+    }
+
+    public static void AddReloadWeapontListener(UnityAction listener_to_register)
+    {
+        reloadWeaponListener = listener_to_register;
+        if (reloadWeaponInvoker != null)
+        {
+            
+            reloadWeaponInvoker.AddResetWeaponListener(reloadWeaponListener);
+        }
+    }
+}
